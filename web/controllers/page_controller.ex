234bd331e09548b3c69256380afb540017eba1d8
Alex Auritt <alexauritt@yahoo.com>
2015-12-31 11:58:51 -0500

    Submit form with no changes to server

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,11 +1,19 @@
 defmodule Luncher.PageController do
   use Luncher.Web, :controller
 
+  def create(conn, _params) do
+    redirect conn, to: page_path(conn, :show, 3)
+  end
+
   def index(conn, _params) do
     render conn, "index.html"
   end
 
   def new(conn, _params) do
     render conn, "new.html"
+  end
+
+  def show(conn, _params) do
+    render conn, "show.html"
   end
 end
